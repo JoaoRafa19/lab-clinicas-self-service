4acede649b5e0de7da459dbda3b99ce0a7599f61
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-02-05 20:37:44 -0300

    feat: Tela de iniciar terminal, e AppBar geral
    Tela inicial do terminal de auto atendimento
    AppBar do sistema

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +6,65 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    final sizeOf = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: Scaffold(
+        appBar: LabClinicasAppBar(
+          actions: [
+            PopupMenuButton(
+              child: IconPopUpMenuWidget(),
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem<int>(
+                    value: 1,
+                    child: Text('Iniciar terminal'),
+                  ),
+                  const PopupMenuItem<int>(
+                    value: 1,
+                    child: Text('Finalizar terminal'),
+                  )
+                ];
+              },
+            )
+          ],
+        ),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: const EdgeInsets.only(top: 112),
+            padding: const EdgeInsets.all(40),
+            width: sizeOf.width * .8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: LabClinicasTheme.orangeColor,
+              ),
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Bem vindo!',
+                  style: LabClinicasTheme.titleStyle,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  width: sizeOf.width * .8,
+                  height: 48,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'INICIAR TERMINAL',
+                      )),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
