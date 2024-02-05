1533adfed3339e3367d9c88a9fcbfe80c23915f7
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-02-04 23:07:51 -0300

    Login e controle de estado com o Signals

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:fe_lab_clinicas_core/lab_clinicas_core_config.dart';
 import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/auth/auth_module.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/home/home_module.dart';
 import 'package:fe_lab_clinicas_self_service/src/pages/splash_page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
 void main() {
-  runApp(const LabClinicasSelfServiceApp());
+  runZonedGuarded(() => runApp(const LabClinicasSelfServiceApp()),
+      (error, stack) {
+    log('Erro não tratado', error: error, stackTrace: stack);
+  });
 }
 
 class LabClinicasSelfServiceApp extends StatelessWidget {
@@ -15,9 +23,12 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return LabClinicasCoreConfig(
       binding: LabClinicasApplicationBinding(),
-
       pageBuilders: [
         FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: "/")
+      ],
+      modules: [
+        AuthModule(),
+        HomeModule(),
       ],
       title: "Lab Clinicas Auto Atendimento",
     );
---
+++ lib/src/modules/auth/login/login_controller.dart
@@ -0,0 +1,34 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/services/user_login_service.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+
+class LoginController with MessageStateMixin {
+  final _obscurePassword = signal(true);
+  bool get obscurePassword => _obscurePassword();
+  void passwordToggle() => _obscurePassword.value = !_obscurePassword.value;
+
+  final _logged = signal(false);
+bool get logged => _logged();
+
+
+
+  final UserLoginService _loginService;
+
+  LoginController({required UserLoginService loginService})
+      : _loginService = loginService;
+
+  Future<void> login(String email, String password) async {
+    final loginResult =
+        await _loginService.execute(email, password).asyncLoader();
+
+    switch (loginResult) {
+      case Left(value: ServiceException(:final message)):
+        showError(message);
+      case Right(value: _):
+        _logged.value = true;
+        
+      // redirecionar
+    }
+  }
+}
