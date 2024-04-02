cd4a4f4d0d8bd9359f2d6ff7452a6209913fbfef
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-04-01 22:30:46 -0300

    Configurando visualização em tempo real da camera

--- lib/src/modules/self_service/documents/scan/documents_scan_page.dart
+++ lib/src/modules/self_service/documents/scan/documents_scan_page.dart
@@ -1,14 +1,125 @@
+import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/widget/lab_clinicas_self_service_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
-class DocumentsScanPage extends StatelessWidget {
+class DocumentsScanPage extends StatefulWidget {
+  const DocumentsScanPage({super.key});
 
-  const DocumentsScanPage({ super.key });
+  @override
+  State<DocumentsScanPage> createState() => _DocumentsScanPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+class _DocumentsScanPageState extends State<DocumentsScanPage> {
+  late CameraController cameraController;
+
+  @override
+  void initState() {
+    cameraController = CameraController(
+        (Injector.get<List<CameraDescription>>().first),
+        ResolutionPreset.ultraHigh);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: LabClinicasSelfServiceAppBar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            width: sizeOf.width * .85,
+            margin: const EdgeInsets.only(top: 18),
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/cam_icon.png'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'TIRAR A FOTO AGORA',
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Posicione o documento dentro do quadro abaixo e presione o botão para tirar a foto',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: LabClinicasTheme.blueColor,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FutureBuilder(
+                  future: cameraController.initialize(),
+                  builder: (context, snapshot) {
+                    switch (snapshot) {
+                      case AsyncSnapshot(
+                          connectionState:
+                              ConnectionState.waiting || ConnectionState.active
+                        ):
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      case AsyncSnapshot(connectionState: ConnectionState.done):
+                        if (cameraController.value.isInitialized) {
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: SizedBox(
+                              width: sizeOf.width * .40,
+                              height: sizeOf.width * .53,
+                              child: CameraPreview(
+                                cameraController,
+                                child: DottedBorder(
+                                  borderType: BorderType.RRect,
+                                  strokeWidth: 4,
+                                  strokeCap: StrokeCap.square,
+                                  color: LabClinicasTheme.orangeColor,
+                                  radius: const Radius.circular(16),
+                                  dashPattern: [1, 10, 1, 3],
+                                  child: const SizedBox.expand(),
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                    }
+                    return const Center(
+                      child: Text('Erro ao carregar câmera'),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                SizedBox(
+                  width: sizeOf.width * .8,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('TIRAR FOTO'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
